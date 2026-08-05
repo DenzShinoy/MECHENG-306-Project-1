e22499963bd3df53378fc5de79df159fde61034e
Your Name <atya162@aucklanduni.ac.nz>
2026-08-05 14:49:12 +1200

    Changed pos command

--- src/GCodeParser.h
+++ src/GCodeParser.h
@@ -5,6 +5,7 @@
 struct GCodeCommand {
  public:
   enum Type : uint8_t { IDLE, MOVE_G1, HOME_G28, FAULT, UNKNOWN };
+  // change a pin / trigger an ISR to then change the state of the FSM
 
   GCodeCommand() = default;
 
