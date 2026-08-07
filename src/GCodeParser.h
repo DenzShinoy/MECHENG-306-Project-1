f25562768f3b67df81fc3f8a8937c4eba9117855
Your Name <atya162@aucklanduni.ac.nz>
2026-08-08 11:07:44 +1200

    added homing draft

--- src/GCodeParser.h
+++ src/GCodeParser.h
@@ -2,6 +2,9 @@
 #include <Arduino.h>
 #include "manager.h"
 
+struct GCodeCommand;
+bool Parser(char* in, GCodeCommand& out);
+
 struct GCodeCommand {
  public:
   enum Type : uint8_t { IDLE, MOVE_G1, HOME_G28, FAULT, UNKNOWN };
@@ -28,9 +31,18 @@ struct GCodeCommand {
   }
 
   void setType(Type type) { type_ = type; }
-  void setX(float value) { x_ = value; hasX_ = true; }
-  void setY(float value) { y_ = value; hasY_ = true; }
-  void setF(float value) { f_ = value; hasF_ = true; }
+  void setX(float value) {
+    x_ = value;
+    hasX_ = true;
+  }
+  void setY(float value) {
+    y_ = value;
+    hasY_ = true;
+  }
+  void setF(float value) {
+    f_ = value;
+    hasF_ = true;
+  }
   void HasX(bool value) { hasX_ = value; }
   void HasY(bool value) { hasY_ = value; }
   void HasF(bool value) { hasF_ = value; }
