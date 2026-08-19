225230dbd67c9a6bc0119118482f0a4deabfc735
louie-bedford <lbed212@aucklanduni.ac.nz>
2026-08-19 12:31:32 +1200

    fixxed updateVelocityProfile1

--- src/updateVelocityProfile1.h
+++ src/updateVelocityProfile1.h
@@ -1,11 +1,8 @@
 #ifndef UPDATE_VELOCITY_PROFILE1_H
 #define UPDATE_VELOCITY_PROFILE1_H
 
-#include "motionPhase.h"
-
 // Function to reset the velocity profile to its initial state
 void resetVelocityProfile1();
-
 
 /** Function to update the velocity profile based on the current phase of motion
  * @param dt Time step depends on pid controller iteration period
@@ -22,22 +19,18 @@ void resetVelocityProfile1();
  * pathVelocity = 0.0; // Starting from rest
  * remainingDistance = 5.0; // 5 millimeters to target
  * --------------------------------------------------------------------
- *  resetVelocityProfile(); -> needed for each new movement command to reset the internal state of the velocity profile
- *  pathVelocity = 0.0f;
+ *  resetVelocityProfile(); -> needed for each new movement command to reset the
+ * internal state of the velocity profile pathVelocity = 0.0f;
  *  .
  *  .
  *  .
  *  .
- *  float updatedVelocity = updateVelocityProfile(dt, cruiseSpeed, acceleration, pathVelocity, remainingDistance);
- *  analogWrite(velocityPin, updatedVelocity * 255.0 / cruiseSpeed); // Scale to PWM range
+ *  float updatedVelocity = updateVelocityProfile(dt, cruiseSpeed, acceleration,
+ * pathVelocity, remainingDistance); analogWrite(velocityPin, updatedVelocity *
+ * 255.0 / cruiseSpeed); // Scale to PWM range
  */
 
+float updateVelocityProfile1(float dt, float cruiseSpeed, float acceleration,
+                             float pathVelocity, float remainingDistance);
 
-float updateVelocityProfile1(
-    float dt, float cruiseSpeed,
-    float acceleration,
-    float pathVelocity,
-    float remainingDistance
-);
-
-#endif // UPDATE_VELOCITY_PROFILE_H
+#endif  // UPDATE_VELOCITY_PROFILE_H
