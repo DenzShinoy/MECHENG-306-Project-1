9813b9188a71a59ac01591917cc25a9aaaae971a
louie-bedford <lbed212@aucklanduni.ac.nz>
2026-08-21 13:30:51 +1200

    added interupt functins

--- src/main.ino
+++ src/main.ino
@@ -2,14 +2,13 @@
 
 #include "Encoder.h"
 #include "Kinematics.h"
+#include "LimitSwitch.h"
 #include "MotorDriver.h"
 #include "PID.h"
 #include "Pins.h"
 #include "Timer.h"
 #include "fsm_1.h"
 #include "manager.h"
-#include "LimitSwitch.h"
-#include "Pins.h"
 
 // Set up encoder and motor driver objects with the correct pins. The encoder
 // ISRs are wired in main() to call the handleEdge() method on each object.
@@ -19,31 +18,24 @@ static Encoder encoderR(pins::ENC_R_A, pins::ENC_R_B);
 static MotorDriver motorL(pins::M1_DIR, pins::M1_PWM, true);
 static MotorDriver motorR(pins::M2_DIR, pins::M2_PWM, true);
 
+void isrLimitTop() { manager.setLimitFault(true); }
+void isrLimitBottom() { manager.setLimitFault(true); }
+void isrLimitLeft() { manager.setLimitFault(true); }
+void isrLimitRight() { manager.setLimitFault(true); }
+
 LimitSwitch swTop(pins::SW_TOP);
 LimitSwitch swBottom(pins::SW_BOTTOM);
 LimitSwitch swLeft(pins::SW_LEFT);
 LimitSwitch swRight(pins::SW_RIGHT);
 
-static Manager manager(
-    swTop,
-    swBottom,
-    swLeft,
-    swRight
-);
+static Manager manager(swTop, swBottom, swLeft, swRight);
 // Set up the G1 motion object with the motor and encoder objects.
 static G1 g1(motorL, motorR, encoderL, encoderR, manager);
 
 // Set up the G28 motion object with the motor and encoder objects.
-static G28 g28(
-    motorL,
-    motorR,
-    encoderL,
-    encoderR,
-    manager
-);
+static G28 g28(motorL, motorR, encoderL, encoderR, manager);
 
 FSM fsm;
-
 
 // Set up the FSM and Manager objects.
 
@@ -98,8 +90,7 @@ void loop() {
 
   // Handle the event and dispatch the current state
   fsm.handleEvent(1);
-  while (true){
-
+  while (true) {
   }
   fsm.dispatch();
 }
